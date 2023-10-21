3916ea4250bc9ede06bedcc7a587c45efc0a0de8
khouwdevin <khouwdevin@gmail.com>
2023-10-21 23:55:11 +0700

    feat(poll): add poll slash command

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -6,33 +6,85 @@ import mongoose from "mongoose";
 const event: BotEvent = {
     name: "messageCreate",
     execute: async (message: Message) => {
-        const temp = message.content.split(' ')
+        if (!message.member || message.member.user.bot) return;
+        if (!message.guild) return;
+        let prefix = process.env.PREFIX
+        if (mongoose.connection.readyState === 1) {
+            let guildPrefix = await getGuildOption(message.guild, "prefix") 
+                if (guildPrefix) prefix = guildPrefix;
+        }
 
-        if (temp[0] === '$help'){
-            message.channel.send(`${message.author}\n\n>>> **$search** : to search username latest post\n**$update** : to continuously update username latest post`)
+        if (!message.content.startsWith(prefix)) return;
+        if (message.channel.type !== ChannelType.GuildText) return;
+
+        let args = message.content.substring(prefix.length).split(" ")
+        let command = message.client.commands.get(args[0])
+
+        if (!command) {
+            let commandFromAlias = message.client.commands.find((command) => command.aliases.includes(args[0]))
+            if (commandFromAlias) command = commandFromAlias
+            else return;
         }
-        else if (temp[0] === '$use'){
-            message.channel.send(`Processing ${message.author}...`)
-            message.channel.send('https://instagram.com/p/Cg9JvnklfSV/')
+
+        let cooldown = message.client.cooldowns.get(`${command.name}-${message.member.user.username}`)
+        let neededPermissions = checkPermissions(message.member, command.permissions)
+        if (neededPermissions !== null)
+            return sendTimedMessage(
+                `
+            You don't have enough permissions to use this command. 
+            \n Needed permissions: ${neededPermissions.join(", ")}
+            `,
+                message.channel,
+                5000
+            )
+
+
+        if (command.cooldown && cooldown) {
+            if (Date.now() < cooldown) {
+                sendTimedMessage(
+                    `You have to wait ${Math.floor(Math.abs(Date.now() - cooldown) / 1000)} second(s) to use this command again.`,
+                    message.channel,
+                    5000
+                )
+                return
+            }
+            message.client.cooldowns.set(`${command.name}-${message.member.user.username}`, Date.now() + command.cooldown * 1000)
+            setTimeout(() => {
+                message.client.cooldowns.delete(`${command?.name}-${message.member?.user.username}`)
+            }, command.cooldown * 1000)
+        } else if (command.cooldown && !cooldown) {
+            message.client.cooldowns.set(`${command.name}-${message.member.user.username}`, Date.now() + command.cooldown * 1000)
         }
-        else if (temp[0] === '$fetch'){
-            const object = await fetch('https://khouwdevin.com/api/randomid', {
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                method: 'POST',
-                body: JSON.stringify({length : 10})
-            })
-            const res = await object.json()
+
+        command.execute(message, args)
+
+        // const temp = message.content.split(' ')
+
+        // if (temp[0] === '$help'){
+        //     message.channel.send(`${message.author}\n\n>>> **$search** : to search username latest post\n**$update** : to continuously update username latest post`)
+        // }
+        // else if (temp[0] === '$use'){
+        //     message.channel.send(`Processing ${message.author}...`)
+        //     message.channel.send('https://instagram.com/p/Cg9JvnklfSV/')
+        // }
+        // else if (temp[0] === '$fetch'){
+        //     const object = await fetch('https://khouwdevin.com/api/randomid', {
+        //         headers: {
+        //             'Content-Type': 'application/json'
+        //         },
+        //         method: 'POST',
+        //         body: JSON.stringify({length : 10})
+        //     })
+        //     const res = await object.json()
     
-            message.channel.send(`${message.author} you're id is **${res.id}**`)
-        }
-        else if (temp[0] === "$hi"){
-            sendTimedMessage(`hello, ${message.author}`, message.channel as TextChannel, 2000)
-        }
-        else if (temp[0] === "$set" || temp[0] === "$s"){
+        //     message.channel.send(`${message.author} you're id is **${res.id}**`)
+        // }
+        // else if (temp[0] === "$hi"){
+        //     sendTimedMessage(`hello, ${message.author}`, message.channel as TextChannel, 2000)
+        // }
+        // else if (temp[0] === "$set" || temp[0] === "$s"){
     
-        } 
+        // } 
     }
 }
 
---
+++ src/slashCommands/poll.ts
@@ -0,0 +1,54 @@
+import { EmbedBuilder, PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
+import { SlashCommand } from "../types";
+
+const command : SlashCommand = {
+    command: new SlashCommandBuilder()
+    .setName("poll")
+    .setDescription("To create polling")
+    .addStringOption(option => option
+        .setName("title")
+        .setDescription("Add title")
+        .setMaxLength(50)
+    )
+    .addStringOption(option => option
+        .setName("option1")
+        .setDescription("Add option 1 of 3")
+        .setMaxLength(150)
+    )
+    .addStringOption(option => option
+        .setName("option2")
+        .setDescription("Add option 2 of 3")
+        .setMaxLength(150)
+    )
+    .addStringOption(option => option
+        .setName("option3")
+        .setDescription("Add option 3 of 3")
+        .setMaxLength(150)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+    execute: async interaction => {
+        await interaction.deferReply({ ephemeral: true })
+        const { channel } = interaction
+        const options = interaction.options.data
+        const emojies = ["1️⃣", "2️⃣", "3️⃣"]
+
+        let embed = new EmbedBuilder()
+            .setTitle(`${options[0].value}`)        
+            .setColor("Blue")
+
+        for (let i = 1; i < options.length; i++)
+            embed.addFields(
+                { name: `${emojies[i-1]} ${options[i].value}`, value: " " }
+            )
+
+        const message = await channel?.send({ embeds: [embed] })
+
+        for (let i = 1; i < options.length; i++)
+            message?.react(emojies[i-1])
+
+        await interaction.editReply("Poll sent successfully!")
+    },
+    cooldown: 2
+}
+
+export default command;
