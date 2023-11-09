be752bf579e05ac3324f2bb93f9daaf2949feb13
khouwdevin <khouwdevin@gmail.com>
2023-11-09 21:18:00 +0700

    fix(node): try to reconnect when connection is lost

--- src/eventsMoon/nodeClose.ts
+++ src/eventsMoon/nodeClose.ts
@@ -8,6 +8,12 @@ const event: MoonEvent = {
         console.log(
             color("text", `❌ Disconnected from ${color("variable", node.host)}`)
         )
+
+        node.connect().then(() => {
+            console.log(
+                color("text", `🔃 Try to reconnect with ${color("variable", node.host)}`)
+            ) 
+        })
     }
 }
 
