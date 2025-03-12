bd54bbe36f35ff80b5e8ae6da44375f9019415f9
khouwdevin <khouwdevin@gmail.com>
2025-03-12 09:19:45 +0700

    fix(slashcommand): fix slashcommand error

--- src/slashCommands/afk.ts
+++ src/slashCommands/afk.ts
@@ -1,73 +1,69 @@
-import {
-  SlashCommandBuilder,
-  GuildMember,
-  PermissionFlagsBits,
-} from "discord.js";
-import { SlashCommand } from "../types";
-import { color } from "../functions";
+import { SlashCommandBuilder, GuildMember } from 'discord.js'
+import { SlashCommand } from '../types'
+import { color } from '../functions'
 
 const command: SlashCommand = {
   command: new SlashCommandBuilder()
-    .setName("afk")
+    .setName('afk')
     .addBooleanOption((options) => {
       return options
-        .setName("afk")
+        .setName('afk')
         .setDescription("Are you afk 'true' or 'false'")
-        .setRequired(true);
+        .setRequired(true)
     })
     .addIntegerOption((options) => {
       return options
-        .setName("time")
-        .setDescription("For how many long? (minutes)")
+        .setName('time')
+        .setDescription('For how many long? (minutes)')
         .setMinValue(1)
-        .setRequired(false);
+        .setRequired(false)
     })
-    .setDefaultMemberPermissions(PermissionFlagsBits.ChangeNickname)
-    .setDescription("Tell your friend if you are going to AFK."),
+    .setDescription('Tell your friend if you are going to AFK.'),
   execute: async (interaction) => {
     try {
-      await interaction.deferReply({ ephemeral: true });
+      await interaction.deferReply({ ephemeral: true })
 
       if (!interaction.channel)
         return console.log(
           color(
-            "text",
-            `❌ Failed to execute AFK slash command : ${color("error", "channel unavailable")}`,
-          ),
-        );
+            'text',
+            `❌ Failed to execute AFK slash command : ${color(
+              'error',
+              'channel unavailable'
+            )}`
+          )
+        )
 
-      const options = interaction.options.data;
-      const minutes: number = (options[1].value as number)
-        ? (options[1].value as number)
-        : 0;
+      const options = interaction.options.data
+      const minutes = options.length > 1 ? options[1].value : 'unknown'
 
-      const userminutesafk = minutes === 0 ? "" : `for ${minutes} minutes`;
-
-      const member = interaction.member as GuildMember;
-      const isAFK = options[0].value as boolean;
+      const member = interaction.member as GuildMember
+      const isAFK = options[0].value as boolean
 
       if (isAFK) {
-        member.setNickname(`[AFK]${member.user.displayName}`).catch(() => {});
         await interaction.channel?.send(
-          `${member} is AFK for ${userminutesafk}`,
-        );
+          `${member} is AFK for ${minutes} minutes`
+        )
+        await member.setNickname(`[AFK]${member.user.displayName}`)
       } else {
-        member
-          .setNickname(member.user.displayName.replace("[AFK]", ""))
-          .catch((e) => {});
-        await interaction.channel?.send(`${member} is not AFK`);
+        await interaction.channel?.send(`${member} is not AFK`)
+        await member.setNickname(member.user.displayName.replace('[AFK]', ''))
       }
 
-      await interaction.editReply("Your command is successfully ran!");
+      await interaction.editReply('Your command is successfully ran!')
     } catch (e) {
+      await interaction.editReply('Error occured!')
       console.log(
         color(
-          "text",
-          `❌ Failed to execute AFK slash command : ${color("error", e.message)}`,
-        ),
-      );
+          'text',
+          `❌ Failed to execute AFK slash command : ${color(
+            'error',
+            e.message
+          )}`
+        )
+      )
     }
   },
-};
+}
 
-export default command;
+export default command
