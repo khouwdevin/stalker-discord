a16cc7c479bcd0427cfa997c977a18a4efacf8b5
khouwdevin <khouwdevin@gmail.com>
2025-04-23 00:52:36 +0700

    fix(logger): fix logger error rendering time

--- src/logger.ts
+++ src/logger.ts
@@ -11,7 +11,7 @@ const formatDate = (): string => {
   const minutes = String(date.getMinutes()).padStart(2, '0')
   const seconds = String(date.getSeconds()).padStart(2, '0')
 
-  return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`
+  return `,"time": "${day}/${month}/${year} ${hours}:${minutes}:${seconds}"`
 }
 
 const logger = pino({
