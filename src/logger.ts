6cb714bbb509d2f59d6f926f0d1e9693724d3a44
khouwdevin <khouwdevin@gmail.com>
2025-04-23 00:41:37 +0700

    update(logger): add date to logger timestamp

--- src/logger.ts
+++ src/logger.ts
@@ -1,8 +1,23 @@
 import pino from 'pino'
 
+const formatDate = (): string => {
+  const date = new Date()
+
+  const day = String(date.getDate()).padStart(2, '0')
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const year = date.getFullYear()
+
+  const hours = String(date.getHours()).padStart(2, '0')
+  const minutes = String(date.getMinutes()).padStart(2, '0')
+  const seconds = String(date.getSeconds()).padStart(2, '0')
+
+  return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`
+}
+
 const logger = pino({
   level: process.env.NODE_ENV === 'production' ? 'info' : 'trace',
   transport: { target: 'pino-pretty', options: { colorize: true } },
+  timestamp: formatDate,
 })
 
 export default logger
