3fd095f6bb50278db780bbb8ab10f56641928420
khouwdevin <khouwdevin@gmail.com>
2025-04-23 09:48:10 +0700

    fix(detectUser): destory player when player is forced to disconnect

--- src/voiceStateFunctions/detectUser.ts
+++ src/voiceStateFunctions/detectUser.ts
@@ -1,34 +1,87 @@
 import { EmbedBuilder, VoiceState } from 'discord.js'
 import logger from '../logger'
 
-const DetectUser = (oldstate: VoiceState, newState: VoiceState) => {
+const DetectUser = async (oldState: VoiceState, newState: VoiceState) => {
   try {
     const client = newState.client
     const player = client.moon.players.get(newState.guild.id)
 
-    if (newState.member && newState.member.user === client.user) {
-      if (!newState.channel) return
-
-      const members = newState.channel.members
-
-      if (members.size > 1) {
-        if (!client.timeouts.has(`player-${player.guildId}`)) return
-
-        if (client.timeouts.has(`player-${player.guildId}`)) {
-          clearTimeout(client.timeouts.get(`player-${player.guildId}`))
-          client.timeouts.delete(`player-${player.guildId}`)
-        }
-
-        return
-      }
-    }
-
+    if (!player) return
     if (
-      !player ||
-      newState.member?.user === client.user ||
-      oldstate.member?.user === client.user
-    )
+      (oldState.member && oldState.member.user === client.user) ||
+      (newState.member && newState.member.user === client.user)
+    ) {
+      if (!newState.channel) {
+        player.stop()
+        player.disconnect()
+        player.destroy()
+
+        await client.moon.players.delete(player.guildId)
+
+        logger.debug(
+          `[Detect User]: Player is destroyed due to client disconnect from ${oldState.channelId}`
+        )
+        logger.trace(
+          `[Detect User]: Delete moon player from guild ${player.guildId}`
+        )
+      } else {
+        const members = newState.channel.members
+
+        if (members.size > 1) {
+          if (client.timeouts.has(`player-${player.guildId}`)) {
+            clearTimeout(client.timeouts.get(`player-${player.guildId}`))
+            client.timeouts.delete(`player-${player.guildId}`)
+          }
+        } else {
+          const timeout = setTimeout(async () => {
+            player.stop()
+            player.disconnect()
+            player.destroy()
+
+            const deleteTimeout = client.timeouts.delete(
+              `player-${player.guildId}`
+            )
+            const deletePlayerAttemps = client.playerAttempts.delete(
+              `player-${player.guildId}`
+            )
+            await client.moon.players.delete(player.guildId)
+
+            logger.trace(
+              `[Detect User]: Delete client timeout ${player.guildId} : ${deleteTimeout}`
+            )
+            logger.trace(
+              `[[Detect User]: Delete player attempts ${player.guildId} : ${deletePlayerAttemps}`
+            )
+            logger.trace(
+              `[Detect User]: Delete moon player from guild ${player.guildId}`
+            )
+
+            const channel = await client.channels
+              .fetch(player.textChannelId)
+              .catch(() => {
+                return logger.error(
+                  '[Detect User]: ❌ Error fetch channel on queueEnd'
+                )
+              })
+
+            if (!channel || !channel.isSendable()) return
+
+            const embed = new EmbedBuilder()
+              .setAuthor({
+                name: 'The new channel does not have users. Player is disconnected',
+                iconURL: client.user?.avatarURL() || undefined,
+              })
+              .setColor('Grey')
+
+            channel.send({ embeds: [embed] })
+          }, 10000)
+
+          client.timeouts.set(`player-${player.guildId}`, timeout)
+        }
+      }
+
       return
+    }
 
     if (newState.channelId === player.voiceChannelId) {
       if (player.playing && client.timeouts.has(`player-${player.guildId}`)) {
@@ -40,16 +93,14 @@ const DetectUser = (oldstate: VoiceState, newState: VoiceState) => {
         `[Detect User]: ${newState.member?.user.tag} joined the same channel as the bot.`
       )
     } else if (
-      oldstate.channelId === player.voiceChannelId &&
+      oldState.channelId === player.voiceChannelId &&
       newState.channelId !== player.voiceChannelId
     ) {
-      if (!oldstate.channel) return
+      if (!oldState.channel) return
 
-      const members = oldstate.channel.members
+      const members = oldState.channel.members
 
       if (members.size > 1) {
-        if (!client.timeouts.has(`player-${player.guildId}`)) return
-
         if (client.timeouts.has(`player-${player.guildId}`)) {
           clearTimeout(client.timeouts.get(`player-${player.guildId}`))
           client.timeouts.delete(`player-${player.guildId}`)
@@ -73,7 +124,7 @@ const DetectUser = (oldstate: VoiceState, newState: VoiceState) => {
           const deletePlayerAttemps = client.playerAttempts.delete(
             `player-${player.guildId}`
           )
-          const deleteMoonPlayer = client.moon.players.delete(player.guildId)
+          await client.moon.players.delete(player.guildId)
 
           logger.trace(
             `[Detect User]: Delete client timeout ${player.guildId} : ${deleteTimeout}`
@@ -82,7 +133,7 @@ const DetectUser = (oldstate: VoiceState, newState: VoiceState) => {
             `[[Detect User]: Delete player attempts ${player.guildId} : ${deletePlayerAttemps}`
           )
           logger.trace(
-            `[Detect User]: Delete moon player ${player.guildId} : ${deleteMoonPlayer}`
+            `[Detect User]: Delete moon player from guild ${player.guildId}`
           )
 
           const channel = await client.channels
@@ -97,7 +148,7 @@ const DetectUser = (oldstate: VoiceState, newState: VoiceState) => {
 
           const embed = new EmbedBuilder()
             .setAuthor({
-              name: 'Users have left. The player is disconnected.',
+              name: 'Users have left. Player is disconnected.',
               iconURL: client.user?.avatarURL() || undefined,
             })
             .setColor('Grey')
@@ -109,7 +160,7 @@ const DetectUser = (oldstate: VoiceState, newState: VoiceState) => {
       }
 
       logger.trace(
-        `[Detect User]: ${oldstate.member?.user.tag} left the bots channel.`
+        `[Detect User]: ${oldState.member?.user.tag} left the bots channel.`
       )
     }
   } catch (e) {
