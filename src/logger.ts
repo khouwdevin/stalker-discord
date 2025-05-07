ed4f11cfc2503f697321a69065c9f5c839c77479
khouwdevin <khouwdevin@gmail.com>
2025-05-07 08:22:15 +0700

    fix(logger): fix logger not showing the right timestamp

--- src/logger.ts
+++ src/logger.ts
@@ -1,23 +1,15 @@
 import pino from 'pino'
 
-const formatDate = (): string => {
-  const date = new Date()
-
-  const day = String(date.getDate()).padStart(2, '0')
-  const month = String(date.getMonth() + 1).padStart(2, '0')
-  const year = date.getFullYear()
-
-  const hours = String(date.getHours()).padStart(2, '0')
-  const minutes = String(date.getMinutes()).padStart(2, '0')
-  const seconds = String(date.getSeconds()).padStart(2, '0')
-
-  return `,"time": "${day}/${month}/${year} ${hours}:${minutes}:${seconds}"`
-}
-
 const logger = pino({
   level: process.env.NODE_ENV === 'production' ? 'info' : 'trace',
-  transport: { target: 'pino-pretty', options: { colorize: true } },
-  timestamp: formatDate,
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      colorizeObject: true,
+      translateTime: 'SYS:yyyy/mm/dd HH:MM:ss',
+    },
+  },
 })
 
 export default logger
